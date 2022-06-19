bf8dd3620cc92f7fae1ec0325b3a272b5e239839
Osaze <osareniyeosazee@gmail.com>
2022-06-19 23:21:11 +0100

    my update page

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -150,9 +150,9 @@ export const Contact = (props) => {
       <div id='footer'>
         <div className='container text-center'>
           <p>
-            &copy; 2020 Issaaf Kattan React Land Page Template. Design by{' '}
-            <a href='http://www.templatewire.com' rel='nofollow'>
-              TemplateWire
+            &copy; 2022 Golden Affilate. Design by{' '}
+            <a href='/' rel='nofollow'>
+              SAZE
             </a>
           </p>
         </div>
--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -7,8 +7,8 @@ export const Gallery = (props) => {
         <div className='section-title'>
           <h2>Gallery</h2>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
-            dapibus leonec.
+            Top Earners
+            
           </p>
         </div>
         <div className='row'>
